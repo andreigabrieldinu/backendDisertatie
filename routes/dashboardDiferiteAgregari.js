93de8b9a9da0556ed4e00a9d67f27c1f7e0cbc2d
andreigabrieldinu <gabidinu@rocketmail.com>
2025-06-04 21:59:11 +0300

    API dashboard client

--- routes/dashboardDiferiteAgregari.js
+++ routes/dashboardDiferiteAgregari.js
@@ -4,8 +4,6 @@ import {
   esteUtilizatorAdmin,
   esteUtilizatorClientSauAdmin,
 } from "./utilizator.js";
-import { getSubscriptie } from "./subscriptie.js";
-import { getIstoricTichet } from "./istoricTichet.js";
 
 const dashboardDiferiteAgregariRouter = express.Router();
 
@@ -40,22 +38,38 @@ const tichetePeStatus = async (statusuri, user) => {
       );
 
       arrayTichetePeStatus.push({
-        status: numeStatus.nume,
-        numarTichete: status._count.idtichet,
+        id: numeStatus.idstatus,
+        label: numeStatus.nume,
+        value: status._count.idtichet,
       });
     }
-    for (const status of statusuri) {
+
+    for (const status of statusurileDinDb) {
       if (
-        !arrayTichetePeStatus.some(
-          (s) => s.status.toLowerCase() === status.toLowerCase()
-        )
+        !arrayTichetePeStatus.some((s) => {
+          s.label === status.nume;
+        })
       ) {
         arrayTichetePeStatus.push({
-          status: status,
-          numarTichete: 0,
+          id: status.idstatus,
+          label: status.nume,
+          value: 0,
         });
       }
     }
+    // sterge duplicatele care au valoarea 0
+    for (let i = 0; i < arrayTichetePeStatus.length; i++) {
+      for (let j = i + 1; j < arrayTichetePeStatus.length; j++) {
+        if (
+          arrayTichetePeStatus[i].label === arrayTichetePeStatus[j].label &&
+          arrayTichetePeStatus[j].value === 0
+        ) {
+          arrayTichetePeStatus.splice(j, 1);
+          j--;
+        }
+      }
+    }
+
     return arrayTichetePeStatus;
   } catch (error) {
     console.error("Eroare la obtinerea tichete pe status:", error);
@@ -63,7 +77,7 @@ const tichetePeStatus = async (statusuri, user) => {
   }
 };
 
-dashboardDiferiteAgregariRouter.get(
+dashboardDiferiteAgregariRouter.post(
   "/tichetePeStatus",
   esteUtilizatorClientSauAdmin,
   async (req, res) => {
@@ -71,9 +85,9 @@ dashboardDiferiteAgregariRouter.get(
       const { statusuri } = { ...req.body };
       const { user } = { ...req };
       if (!statusuri || !Array.isArray(statusuri) || statusuri.length === 0) {
-        return res.status(404).json({
-          message: "Nu s-au furnizat statusuri sau formatul este incorect",
-        });
+        return res
+          .status(200)
+          .json([{ status: "Nu s-au selectat statusuri", numarTichete: 0 }]);
       }
       let nrTichetePeStatusuri = await tichetePeStatus(statusuri, user);
       res.json(nrTichetePeStatusuri);
@@ -133,8 +147,8 @@ const getTicheteInchiseSiDeschise = async (user) => {
     }, 0);
 
     let final = {
-      "Tichete deschise": totalDeschise,
-      "Tichete inchise": totalInchise,
+      ticheteDeschise: totalDeschise,
+      ticheteInchise: totalInchise,
     };
     return final;
   } catch (error) {
@@ -173,13 +187,23 @@ const getTicheteDeschisePeZile = async (user) => {
         datacreare: "asc",
       },
     });
-    //formateaza ticheteDeschisePeZile pentru a avea data in format YYYY-MM-DD si de forma {"nrTichete": 5, "data": "2023-10-01"}
     ticheteDeschisePeZile = ticheteDeschisePeZile.map((tichet) => {
       return {
         nrTichete: tichet._count.idtichet,
-        data: tichet.datacreare.toISOString().split("T")[0], // formateaza data in YYYY-MM-DD
+        data: tichet.datacreare.toISOString().split("T")[0],
       };
     });
+    //grupeaza tichetele pe zile
+    for (let i = 0; i < ticheteDeschisePeZile.length; i++) {
+      for (let j = i + 1; j < ticheteDeschisePeZile.length; j++) {
+        if (ticheteDeschisePeZile[i].data === ticheteDeschisePeZile[j].data) {
+          ticheteDeschisePeZile[i].nrTichete +=
+            ticheteDeschisePeZile[j].nrTichete;
+          ticheteDeschisePeZile.splice(j, 1);
+          j--;
+        }
+      }
+    }
 
     if (ticheteDeschisePeZile.length === 0) {
       return [];
@@ -192,12 +216,16 @@ const getTicheteDeschisePeZile = async (user) => {
 };
 
 dashboardDiferiteAgregariRouter.get(
-  "/ticheteDeschisePeZile",
+  "/ticheteDeschisePeZileAllTime",
   esteUtilizatorClientSauAdmin,
   async (req, res) => {
     try {
       const { user } = { ...req };
-      let ticheteDeschisePeZile = await getTicheteDeschisePeZile(user);
+      let ticheteDeschisePeZile = await getTicheteDeschisePeZile(
+        user,
+        startDate,
+        endDate
+      );
       if (ticheteDeschisePeZile.length === 0) {
         return res.status(404).json({
           message: "Nu exista tichete deschise pe zile pentru acest utilizator",
@@ -206,6 +234,129 @@ dashboardDiferiteAgregariRouter.get(
       res.json(ticheteDeschisePeZile);
     } catch (error) {
       console.error("Eroare la obtinerea tichete deschise pe zile:", error);
+      res.status(500).json({ message: "Eroare la obtinerea datelor" });
+    }
+  }
+);
+
+const getTicheteDeschisePeZileInterval = async (user, startDate, endDate) => {
+  try {
+    let ticheteDeschisePeZile = await prisma.tichet.groupBy({
+      by: ["datacreare"],
+      _count: {
+        idtichet: true,
+      },
+      where: {
+        idutilizator: user.idutilizator,
+        idstatus: { in: [1, 2, 3, 4, 5] },
+        datacreare: {
+          gte: new Date(startDate),
+          lte: new Date(endDate),
+        },
+      },
+      orderBy: {
+        datacreare: "asc",
+      },
+    });
+    ticheteDeschisePeZile = ticheteDeschisePeZile.map((tichet) => {
+      return {
+        nrTichete: tichet._count.idtichet,
+        data: tichet.datacreare.toISOString().split("T")[0],
+      };
+    });
+    //grupeaza tichetele pe zile
+    for (let i = 0; i < ticheteDeschisePeZile.length; i++) {
+      for (let j = i + 1; j < ticheteDeschisePeZile.length; j++) {
+        if (ticheteDeschisePeZile[i].data === ticheteDeschisePeZile[j].data) {
+          ticheteDeschisePeZile[i].nrTichete +=
+            ticheteDeschisePeZile[j].nrTichete;
+          ticheteDeschisePeZile.splice(j, 1);
+          j--;
+        }
+      }
+    }
+    if (ticheteDeschisePeZile.length === 0) {
+      return [];
+    }
+    return ticheteDeschisePeZile;
+  } catch (error) {
+    console.error("Eroare la obtinerea tichete deschise pe zile:", error);
+    throw new Error("Eroare la obtinerea datelor");
+  }
+};
+
+dashboardDiferiteAgregariRouter.get(
+  "/ticheteDeschisePeZileInterval/startDate/:startDate/endDate/:endDate",
+  esteUtilizatorClientSauAdmin,
+  async (req, res) => {
+    try {
+      const { user } = { ...req };
+      const { startDate, endDate } = req.params;
+
+      if (!startDate || !endDate) {
+        return res.status(400).json({
+          message: "Datele de inceput si final sunt necesare",
+        });
+      }
+      if (new Date(startDate) > new Date(endDate)) {
+        return res.status(400).json({
+          message: "Data de inceput trebuie sa fie inainte de data de final",
+        });
+      }
+      let ticheteDeschisePeZile = await getTicheteDeschisePeZileInterval(
+        user,
+        startDate,
+        endDate
+      );
+      if (ticheteDeschisePeZile.length === 0) {
+        return res.status(404).json({
+          message: "Nu exista tichete deschise pe zile in acest interval",
+        });
+      }
+      res.json(ticheteDeschisePeZile);
+    } catch (error) {
+      console.error("Eroare la obtinerea tichete deschise pe zile:", error);
+      res.status(500).json({ message: "Eroare la obtinerea datelor" });
+    }
+  }
+);
+
+dashboardDiferiteAgregariRouter.get(
+  "/produsePeTichete",
+  esteUtilizatorClientSauAdmin,
+  async (req, res) => {
+    try {
+      const { user } = { ...req };
+      const produsePeTichete = await prisma.tichet.groupBy({
+        by: ["produs"],
+        _count: {
+          idtichet: true,
+        },
+        where: {
+          idutilizator: user.idutilizator,
+          idstatus: { in: [1, 2, 3, 4, 5] },
+        },
+        orderBy: {
+          produs: "asc",
+        },
+      });
+
+      let produsePeTicheteFinal = produsePeTichete.map((tichet) => {
+        return {
+          label: tichet.produs,
+          value: tichet._count.idtichet,
+        };
+      });
+
+      if (produsePeTicheteFinal.length === 0 || !produsePeTicheteFinal) {
+        return res.status(404).json({
+          message: "Nu exista produse asociate cu tichetele acestui utilizator",
+        });
+      }
+
+      res.json(produsePeTicheteFinal);
+    } catch (error) {
+      console.error("Eroare la obtinerea produselor pe tichete:", error);
       res.status(500).json({ message: "Eroare la obtinerea datelor" });
     }
   }
